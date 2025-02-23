fca6e3c5a45cbeeb9a52ec155a038f8e6db7bbe2
fdevi3-mle <fdevi3@proton.me>
2025-02-23 23:48:12 +0100

    Done finally day 1

--- src/train_model.py
+++ src/train_model.py
@@ -1,18 +1,15 @@
-from typing import Tuple,Annotated
 import os
-import pandas as pd
-import joblib
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from zenml import step, ArtifactConfig
-from zenml.logger import get_logger
 #  Warnings
 import warnings
-from AutoClean import AutoClean
+from typing import Annotated
 
-from utils import CSV_PATH, PROCESSED_PATH, get_data, get_csv_path
+import bentoml
+from bentoml import bentos
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.model_selection import RandomizedSearchCV
+from zenml import step
+from zenml.integrations.bentoml.steps import bento_builder_step
 
 warnings.filterwarnings('ignore')
 # Set random state
@@ -22,7 +19,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 ##I assume you want to load the csv but
 @step
-def train_model_bento(X_train, X_test, y_train, y_test):
+def train_model_bento(X_train, X_test, y_train, y_test)->Annotated[RandomForestRegressor,"RandomForestRegressorBento"]:
 
     #Just use a grid search for the best regresrro
     params = {
@@ -45,3 +42,18 @@ def train_model_bento(X_train, X_test, y_train, y_test):
     print(f"R-squared Score {r2}")
     print(f"Mean Squared Erros {mse}")
     print(f"Mean Absolute Error {mae}")
+
+    bento_model = bentoml.sklearn.save_model(
+        "admission_model",
+        best_est,
+        custom_objects={
+            "feature_names": X_train.columns.tolist(),
+            "target_name": "Chance of Admit"
+        },
+        metadata={
+            "version": "v1",
+            "best_params": random_search.best_params_,
+            "best_score": random_search.best_score_
+        }
+    )
+    return best_est
--- zenml-main.py
+++ zenml-main.py
@@ -3,6 +3,8 @@ from zenml import pipeline, ArtifactConfig
 from zenml.client import Client
 from src.prepare_data import bento_data_loader, bento_data_processor, bento_data_splitter
 from src.train_model import train_model_bento
+from zenml.integrations.bentoml.steps import bento_builder,bentoml_model_deployer_step
+
 
 # Zenml Client
 client = Client()
@@ -15,7 +17,8 @@ def bentoml_pipeline():
     dataframe = bento_data_loader()
     processed_data = bento_data_processor(dataframe)
     X_train,X_test,y_train,y_test = bento_data_splitter(processed_data)
-    train_model_bento(X_train,X_test,y_train,y_test)
+    model=train_model_bento(X_train,X_test,y_train,y_test)
+
 
 
 if __name__ == "__main__":
